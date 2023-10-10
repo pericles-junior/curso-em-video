7a78ae0fe2e8b06b76a9f6e1886f67d97ce8f03f
Perinho <periclesmjr@hotmail.com>
2023-10-10 17:00:36 -0300

    aprendendo while e for

---
+++ Curso em Vídeo - JS/aula13/ambiente.js
@@ -0,0 +1,11 @@
+let count = 1
+do {
+    console.log(`${count}. Oi tudo bem?`)
+    count++
+} while (count <= 6) 
+
+/* let count = 1
+while (count <= 6) {
+    console.log(`${count}. Oi tudo bem?`)
+    count++
+} */
